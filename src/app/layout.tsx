8d2e705fc877b12aafa1b9e4f99ce192bf5fa798
dubaiwebcity <dubaiwebcity@gmail.com>
2026-04-08 17:44:44 +0400

    fix: gtm code (#381)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,55 +1,87 @@
-import { GoogleTagManager } from '@next/third-parties/google';
 import Script from "next/script";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ar" dir="rtl">
-      <body>
-
-        {/* ✅ Google Tag Manager (must be at top of body) */}
-        <GoogleTagManager gtmId="GTM-NVPZTJNP" />
+      <head>
+        {/* ✅ Google Tag Manager */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){
+                w[l]=w[l]||[];
+                w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+                var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+                j.async=true;
+                j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+                f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-NVPZTJNP');
+            `,
+          }}
+        />
 
         {/* ✅ Facebook Pixel */}
-        <Script strategy="afterInteractive" id="facebook-pixel">
-          {`
-            !function(f,b,e,v,n,t,s){
-            if(f.fbq)return;
-            n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;
-            n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];
-            t=b.createElement(e);t.async=!0;
-            t.src=v;
-            s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)
-            }(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
+        <Script
+          id="facebook-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s){
+              if(f.fbq)return;
+              n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;
+              n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];
+              t=b.createElement(e);t.async=!0;
+              t.src=v;
+              s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)
+              }(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
 
-            fbq('init', '807341019046878');
-            fbq('track', 'PageView');
-          `}
-        </Script>
+              fbq('init', '807341019046878');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
 
         {/* ✅ TikTok Pixel */}
-        <Script strategy="afterInteractive" id="tiktok-pixel">
-          {`
-            !function (w, d, t) {
-              w.TiktokAnalyticsObject=t;
-              var ttq=w[t]=w[t]||[];
-              ttq.methods=["page","track","identify","ready"];
-              ttq.load=function(e){
-                var s=d.createElement("script");
-                s.src="https://analytics.tiktok.com/i18n/pixel/events.js?sdkid="+e+"&lib="+t;
-                s.async=true;
-                d.getElementsByTagName("head")[0].appendChild(s);
-              };
+        <Script
+          id="tiktok-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function (w, d, t) {
+                w.TiktokAnalyticsObject=t;
+                var ttq=w[t]=w[t]||[];
+                ttq.methods=["page","track","identify","ready"];
+                ttq.load=function(e){
+                  var s=d.createElement("script");
+                  s.src="https://analytics.tiktok.com/i18n/pixel/events.js?sdkid="+e+"&lib="+t;
+                  s.async=true;
+                  d.getElementsByTagName("head")[0].appendChild(s);
+                };
+                ttq.load('D6ES013C77UAAN0097SG');
+              }(window, document, 'ttq');
+            `,
+          }}
+        />
+      </head>
 
-              ttq.load('D6ES013C77UAAN0097SG');
-              // ttq.page();  // enable later if needed
-            }(window, document, 'ttq');
-          `}
-        </Script>
+      <body>
+        {/* ✅ GTM NoScript (important) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NVPZTJNP"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
 
         {children}
       </body>
