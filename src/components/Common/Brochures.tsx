7bf16f11f578ae83170d1102a71608e27517a8d3
dubaiwebcity <dubaiwebcity@gmail.com>
2026-04-09 17:05:49 +0400

    fix: change desgin (#385)

--- src/components/Common/Brochures.tsx
+++ src/components/Common/Brochures.tsx
@@ -12,6 +12,8 @@ const brochures = [
       {
         title_en: "POST-OPERATIVE INSTRUCTIONS: EGG RETRIEVAL",
         title_ar: "تـعـلـيـمات ما بعد سحـب البـويـضـات",
+        size: "2.8 MB",
+        updated: "Oct 2026",
         image: "https://bnoon-website.b-cdn.net/images/brochures/post-egg-retrieval.jpg",
         pdf: "/en/egg-retrieval-post-operative",
         downloadpdf: "/pdf/post-operative-instructions-egg-retrieval.pdf",
@@ -19,6 +21,8 @@ const brochures = [
       {
         title_en: "PRE-OPERATIVE INSTRUCTIONS: EGG RETRIEVAL",
         title_ar: "تعـليـمـات مـا قبل سحـب البـويـضـات ",
+        size: "2.1 MB",
+        updated: "Oct 2026",
         image: "https://bnoon-website.b-cdn.net/images/brochures/pre-egg-retrieval.jpg",
         pdf: "/en/egg-retrieval-pre-operative",
         downloadpdf: "/pdf/pre-operative-instructions-egg-retrieval.pdf",
@@ -33,6 +37,8 @@ const brochures = [
       {
         title_en: "POST-OPERATIVE INSTRUCTIONS: EMBRYO TRANSFER",
         title_ar: "تـعـلـيـمات ما بعد إرجـــاع األجـــنـــة",
+         size: "4.5 MB",
+        updated: "Jan 2026",
         image: "https://bnoon-website.b-cdn.net/images/brochures/post-embryo-transfer.jpg",
         pdf: "/en/embryo-transfer-post-operative",
         downloadpdf: "/pdf/post-operative-instructions-enbryo-transfer.pdf",
@@ -40,6 +46,8 @@ const brochures = [
       {
         title_en: "PRE-OPERATIVE INSTRUCTIONS: EMBRYO TRANSFER ",
         title_ar: "تعـليـمـات مـا قبل إرجـــاع األجـــنـــة",
+        size: "494 KB",
+        updated: "Jan 2026",
         image: "https://bnoon-website.b-cdn.net/images/brochures/pre-embryo-transfer.jpg",
         pdf: "/en/embryo-transfer-pre-operative",
         downloadpdf: "/pdf/pre-operative-instructions-enbryo-transfer.pdf",
@@ -53,6 +61,8 @@ const brochures = [
       {
         title_en: "SEMEN COLLECTION INSTRUCTIONS",
         title_ar: "تعليمات جمع عينة السائل المنوي",
+        size: "7.0 MB",
+        updated: "Dec 2025",
         image: "https://bnoon-website.b-cdn.net/images/brochures/semen.jpg",
         pdf: "/en/semen-collection-instructions",
         downloadpdf: "/pdf/semen-collection-instructions.pdf",
@@ -68,7 +78,7 @@ const Brochures = () => {
   };
 
   return (
-    <div className="fertility-area bg-color" dir="rtl">
+    <div className="fertility-area bg-color">
       <div className="container mt-5 mb-5 ">
         {brochures.map((section, i) => (
           <div key={i} className="section-title mt-5 mb-5">
@@ -80,52 +90,57 @@ const Brochures = () => {
 
 
             </div>
-            <div className="row g-5 justify-content-center">
-              {section.items.map((item, index) => (
-                <div className="col-lg-5 col-md-6" key={index}>
-                  <motion.div
-                    variants={cardAnimation}
-                    initial="hidden"
-                    whileInView="visible"
-                    transition={{ duration: 0.5 }}
-                  >
-                    <div className="brochure-card p-3">
-                      <div className="brochure-image mb-3">
-                        <img
-                          src={item.image}
-                          alt={item.title_en}
-                          className="img-fluid rounded"
-                        />
-                      </div>
-                      <div className="text-center">
-
-                        <a href={item.pdf} className="text-link">
-                          <h6 className="fw-semibold mb-1 brochures">{item.title_ar}</h6>
-                          <h6 className="fw-semibold mb-3 brochures">{item.title_en}</h6>
-                        </a>
-
-                        <a
-                          href={item.downloadpdf}
-                          target="_blank"
-                          rel="noopener noreferrer"
-                          className="download-btn"
-                        >Download PDF
-                          <img src="https://bnoon-website.b-cdn.net/images/icons/download-icon.png" alt="download" />
-
-                        </a>
-                      </div>
-                    </div>
-                  </motion.div>
-                </div>
-              ))}
-            </div>
+       <div className="row g-5 justify-content-center">
+                   {section.items.map((item, index) => (
+                     <div className="col-lg-4 col-md-4" key={index}>
+                       <motion.div
+                         variants={cardAnimation}
+                         initial="hidden"
+                         whileInView="visible"
+                         transition={{ duration: 0.5 }}
+                       >
+                         <div className="brochure-card">
+                           {/* IMAGE */}
+                           <div className="image-wrapper">
+                             <img src={item.image} alt={item.title_en} />
+     
+                            
+                           </div>
+     
+                           {/* CONTENT */}
+                           <div className="card-content">
+      <h3>{item.title_en}</h3>
+                             <h4 className="text-font-style">{item.title_ar}</h4>
+                            
+                             {/* FOOTER */}
+                             <div className="card-footer">
+                               <div>
+                                 <span>FILE SIZE</span>
+                                 <strong>{item.size}</strong>
+                               </div>
+     
+                               <div>
+                                 <span>UPDATED</span>
+                                 <strong>{item.updated}</strong>
+                               </div>
+     
+                               <a href={item.downloadpdf} target="_blank" className="download-icon">
+                                 <img src="https://bnoon-website.b-cdn.net/images/icons/download-file-icon.svg" alt="download" />
+                               </a>
+                             </div>
+                           </div>
+                         </div>
+                       </motion.div>
+                     </div>
+                   ))}
+                 </div>
           </div>
         ))}
       </div>
 
       {/* CSS inside same file */}
-      <style jsx global>{`
-  
+      <style jsx>{`
+   
       `}</style>
     </div>
   );
