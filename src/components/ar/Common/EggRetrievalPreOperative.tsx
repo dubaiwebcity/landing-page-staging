d37859fc89be575774c351bd3ba85997457a5f77
dubaiwebcity <dubaiwebcity@gmail.com>
2026-04-10 17:12:00 +0400

    fix: change text (#397)

--- src/components/ar/Common/EggRetrievalPreOperative.tsx
+++ src/components/ar/Common/EggRetrievalPreOperative.tsx
@@ -10,17 +10,44 @@ const PaitentRights: React.FC = () => {
   ];
 
   return (
-    <div className="pdf-pages-wrapper">
-      {pages.map((page, index) => (
-        <OptimizedImage
-          key={index}
-          imageName={page.imageName}
-          alt={page.alt}
-          className="pdf-page"
-        />
-      ))}
+<div className="pdf-container">
 
+  {/* 🔽 Download Button (logo ke niche, right aligned) */}
+  <div className="download-wrapper">
+    <a
+      href="/pdf/pre-operative-instructions-egg-retrieval.pdf"
+      target="_blank"
+      className="download-icon-img"
+    >
+      <img
+        src="https://bnoon-website.b-cdn.net/images/icons/download-file-icon.svg"
+        alt="download"
+      />
+    </a>
+  </div>
+
+  <div className="pdf-pages-wrapper">
+    {pages.map((page, index) => (
+      <OptimizedImage
+        key={index}
+        imageName={page.imageName}
+        alt={page.alt}
+        className="pdf-page"
+      />
+    ))}
+  </div>
       <style jsx>{`
+      .download-wrapper {
+  width: 100%;
+  padding: 20px 320px 0;
+}
+
+.download-icon-img img {
+  transition: 0.3s;
+  width: 25px;
+}
+
+
         .pdf-pages-wrapper {
           width: 100%;
           display: flex;
