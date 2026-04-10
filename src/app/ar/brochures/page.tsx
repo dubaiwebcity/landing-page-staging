f5e3650d5f44691e75cd6cbbca6d459ab3f33f97
dubaiwebcity <dubaiwebcity@gmail.com>
2026-04-10 16:40:33 +0400

    fix: update banner text (#394)

--- src/app/ar/brochures/page.tsx
+++ src/app/ar/brochures/page.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import OptimizedPageBanner from '@/components/ui/OptimizedPageBanner';
 import Navbar from '@/components/ar/Layout/Navbar';
 import Brochures from '@/components/ar/Common/Brochures';
-
+import FoundingPageBanner from "@/components/ar/Layout/FoundingPageBanner";
 export const metadata: Metadata = {
   title: 'نبذة عنّا – بنون',
   description:
@@ -13,7 +13,43 @@ export default function AboutUsPage() {
   return (
     <>
       <Navbar />
-      <OptimizedPageBanner imageName="aboutus-banner-ar" />
+<div style={{ position: "relative" }}>
+
+  <FoundingPageBanner bgImage="https://bnoon-website.b-cdn.net/images/banner/ar/brochures-banner-ar.jpg" />
+
+  {/* ✅ FIX: ensure overlay is above image */}
+  <div
+    style={{
+      position: "absolute",
+      inset: 0,
+      backgroundColor: "#004d6754",
+      zIndex: 5,
+      pointerEvents: "none",
+    }}
+  />
+
+  <div className="container">
+    <div
+      className="second-banner-content reveal-text text-banner"
+      style={{
+        position: "absolute",
+        top: "50%",
+        transform: "translateY(-50%)",
+        zIndex: 20,
+      }}
+    >
+      <h1 style={{ color: "#ffffff" }}>
+        <span className="rowdies-font text-size">
+          الموارد والكتيبات للمرضى
+        </span>
+      </h1>
+
+      <p style={{ color: "#fff" }} className="special-desc"></p>
+      <p style={{ color: "#fff" }} className="terms-text"></p>
+    </div>
+  </div>
+
+</div>
       <Brochures />
     </>
   );
