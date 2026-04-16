34b345fb57191ae19665c619b5e94dc859b011b4
dubaiwebcity <dubaiwebcity@gmail.com>
2026-04-16 11:16:45 +0400

    fix: remove css (#429)

--- src/components/Layout/FoundingPageBanner.tsx
+++ src/components/Layout/FoundingPageBanner.tsx
@@ -16,35 +16,7 @@ export default function FoundingDayBanner({ bgImage }: Props) {
 
       <style>{`
 
-       .founding-banner-area {
-          width: 100%;
-          height: 360px;
-          background-size: cover;
-          background-position: center;
-          background-repeat: no-repeat;
-        }
-          .text-size{
-              font-size: 44px !important;
-          }
-                   .text-banner{
-                   max-width: 50% !important;
-              }
-        @media (max-width: 768px) {
-          .founding-banner-area {
-            height: 100px;
-            background-position: 100% center;
-          }
-                .text-size {
-        font-size: 13px !important;
-        font-weight: 800;
-        line-height: 1.4;
-    }
-              .text-banner{
-               left: 4% !important;
-                  top: 56% !important;
-                  width: 50%;
-              }
-        }
+       
       `}</style>
     </>
   );
