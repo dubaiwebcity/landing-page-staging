3979b57e171cf68daf0018db4d75b9d1a308772a
dubaiwebcity <dubaiwebcity@gmail.com>
2026-04-20 13:46:28 +0400

    fix: Update route.js (#462)

--- src/app/api/referrals-ar/route.js
+++ src/app/api/referrals-ar/route.js
@@ -27,7 +27,7 @@ export async function POST(req) {
    const RECIPIENTS = {
   "بنـــون – جدة": "referral.jeddah@bnoon.sa",
   "بنـــون – الرياض": "referral.riyadh@bnoon.sa",
-  "بنـــون – الأحساء": "referral.alahsa@bnoon.sa",
+  "بنـــون – الأحساء": "zulaikhakhalid18@gmail.com",
 };
 
     // default fallback (optional)
